30e45a08868f7decdf53eba670bd68e581967c72
prakash150499 <121739207+prakash150499@users.noreply.github.com>
2023-09-04 07:30:51 +0530

    last try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,11 +17,11 @@ public class Main {
     }
     public static void main(String[]args)
     {
-        B obj = new B();
-        obj.meth();
+           B obj = new B();
+           obj.meth();
 
-        B obj1 = new B();
-        obj1.meth();
+           B obj1 = new B();
+           obj1.meth();
 
     }
 }
